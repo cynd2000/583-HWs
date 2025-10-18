b0c269d8977eab5bdd924baa69a6281a21f8a936
cynd2000 <150089830+cynd2000@users.noreply.github.com>
2025-10-18 22:07:25 +0100

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -1,12 +1,20 @@
 import requests
 import json
 
+PINATA_API_KEY = "4b5abe769c0043a1c92c"
+PINATA_API_SECRET = "89d3118f2dd90d867235ec423cc94e274789dcc0add5c3cce4c5bdc9255c8e71"
+
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	json_data = json.dumps(data)
 
+	headers = {
+		"pinata_api_key": PINATA_API_KEY,
+		"pinata_secret_api_key": PINATA_API_SECRET
+	}
+	
 	response = requests.post(
-		"https://ipfs.infura.io:5001/api/v0/add", files={"file": ("data.json", json_data)}  
+		"https://api.pinata.cloud/pinning/pinFileToIPFS", files={"file": ("data.json", json_data), headers=headers}  
 	)
 
 	response.raise_for_status()
@@ -19,9 +27,8 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 
-	response = requests.post(
-		"https://ipfs.infura.io:5001/api/v0/cat", params={"arg": cid}
-	)
+	url = f"https://gateway.pinata.cloud/ipfs/{cid}"
+	response = requests.get(url)
 
 	response.raise_for_status()
 	data = response.json()
