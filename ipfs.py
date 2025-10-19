ce8b8af8f28978a345b7964f4f9ce3991ab32acb
cynd2000 <150089830+cynd2000@users.noreply.github.com>
2025-10-19 15:52:18 +0100

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -1,30 +1,24 @@
 import requests
 import json
-import io
 
 PINATA_API_KEY = "b1c8c5542c984e1c5ff4"
 PINATA_API_SECRET = "cb692f00a1d60f0979e796b51386d2371bd1ae21ae497659e7eca0d71bc38f9b"
 
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
-	json_bytes = io.BytesIO(json.dumps(data).encode("utf-8"))
-
+	url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+	payload = {"pinataContent": data} 
 	headers = {
 		"pinata_api_key": PINATA_API_KEY,
 		"pinata_secret_api_key": PINATA_API_SECRET
 	}
 	
-	r = requests.get("https://api.pinata.cloud/data/testAuthentication", headers=headers)
-	print(r.status_code, r.text)
-	
-	response = requests.post(
-		"https://api.pinata.cloud/pinning/pinJSONToIPFS", files={"file": ("data.json", json_bytes)}, headers=headers
-	)
+	response = requests.post(url, json=payload, headers=headers)
 
 	response.raise_for_status()
 	result = response.json()
 
-	cid = result["Hash"]
+	cid = result["IpfsHash"]
 	return cid
 
 
