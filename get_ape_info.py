d4b4ecb3faf1177da24a6f26c03d140743d64322
cynd2000 <150089830+cynd2000@users.noreply.github.com>
2025-10-23 21:52:31 +0100

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -30,7 +30,7 @@ def get_ape_info(ape_id):
     # YOUR CODE HERE
     data['owner'] = contract.functions.ownerOf(ape_id).call()
     tokenURI = contract.functions.tokenURI(ape_id).call()
-    ipfs_link = 'https://ipfs.io/ipfs/' + tokenURI['image'][7:]
+    ipfs_link = 'https://ipfs.io/ipfs/' + tokenURI[7:]
 
     response = requests.get(ipfs_link)
     metadata = response.json()
