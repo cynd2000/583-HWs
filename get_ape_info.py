015634354003b217984787a5064d2015158ec11c
cynd2000 <150089830+cynd2000@users.noreply.github.com>
2025-10-23 21:37:41 +0100

    Add files via upload

---
+++ get_ape_info.py
@@ -0,0 +1,42 @@
+from web3 import Web3
+from web3.providers.rpc import HTTPProvider
+import requests
+import json
+
+bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
+contract_address = Web3.to_checksum_address(bayc_address)
+
+# You will need the ABI to connect to the contract
+# The file 'abi.json' has the ABI for the bored ape contract
+# In general, you can get contract ABIs from etherscan
+# https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+with open('ape_abi.json', 'r') as f:
+    abi = json.load(f)
+
+############################
+# Connect to an Ethereum node
+api_url = "https://mainnet.infura.io/v3/b7c32404a94c4097b835726644af9699"  # YOU WILL NEED TO PROVIDE THE URL OF AN ETHEREUM NODE
+provider = HTTPProvider(api_url)
+web3 = Web3(provider)
+contract = w3.eth.contract(address=contract_address, abi=abi)
+
+def get_ape_info(ape_id):
+    assert isinstance(ape_id, int), f"{ape_id} is not an int"
+    assert 0 <= ape_id, f"{ape_id} must be at least 0"
+    assert 9999 >= ape_id, f"{ape_id} must be less than 10,000"
+
+    data = {'owner': "", 'image': "", 'eyes': ""}
+
+    # YOUR CODE HERE
+    data['owner'] = contract.functions.ownerOf(ape_id).call()
+    data['image'] = contract.functions.tokenURI(ape_id).call()
+    ipfs_link = 'https://ipfs.io/ipfs' + data['image'][7:]
+
+    response = requests.get(ipfs_link)
+    metadata = response.json()
+    data['eyes'] = next((t["value"] for t in metadata.get("attributes", []) if t["trait_type"] == "Eyes"), None)
+
+    assert isinstance(data, dict), f'get_ape_info{ape_id} should return a dict'
+    assert all([a in data.keys() for a in
+                ['owner', 'image', 'eyes']]), f"return value should include the keys 'owner','image' and 'eyes'"
+    return data
