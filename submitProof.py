709b58f724332222ea0b3998ef9da207a22ab6b0
cynd2000 <150089830+cynd2000@users.noreply.github.com>
2025-11-06 21:10:03 +0000

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -6,6 +6,7 @@ import math
 from pathlib import Path
 from web3 import Web3
 from web3.middleware import ExtraDataToPOAMiddleware  # Necessary for POA chains
+from eth_account.messages import encode_defunct
 
 
 def merkle_assignment():
